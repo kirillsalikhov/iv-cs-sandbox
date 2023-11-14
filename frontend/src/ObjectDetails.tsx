c47d863bf2d16a0196c0d816489a33be9530090f
Max Ustinov <ewclide@yandex.ru>
2023-11-14 20:01:58 +0400

    updated details output

--- frontend/src/ObjectDetails.tsx
+++ frontend/src/ObjectDetails.tsx
@@ -12,14 +12,14 @@ export interface ObjectDetailsProps {
 
 export function ObjectDetails({ info }: ObjectDetailsProps): ReactNode {
     return (
-        <div className={'absolute w-1/4 right-16 top-16 bg-white border border-black'}>
+        <div className={'absolute w-1/3 right-16 top-16 bg-white border border-black'}>
             <h1 className={'p-4 border-b border-black'}>{info.name}</h1>
             <ul>
                 {Object.entries(info.properties).map(([pname, pvalue]) => {
                     return (
                         <li key={pname} className={'flex border-b border-gray'}>
                             <div className={'py-2 px-4 w-1/2 border-r border-gray'}>{pname}</div>
-                            <div className={'py-2 px-4'}>{pvalue}</div>
+                            <div className={'py-2 px-4 w-1/2 break-words'}>{pvalue}</div>
                         </li>
                     )
                 })}
--- frontend/src/object-details.ts
+++ frontend/src/object-details.ts
@@ -1,17 +1,16 @@
 import WofDB from '@wg/objects-db';
 import { ObjectInfo, ObjectProps } from './ObjectDetails';
 
+const exceptions = ['_id', 'children'];
+
 export async function getObjectDetails(id: number, db: WofDB): Promise<ObjectInfo> {
     const response = await db.request([id]);
     const { Name: name, ...props } = response[0];
 
     const properties: ObjectProps = {};
     for (const [n, v] of Object.entries(props)) {
-        if (Array.isArray(v)) {
-            properties[n] = `[${v.join(', ')}]`
-        } else {
-            properties[n] = String(v);
-        }
+        if (exceptions.includes(n)) { continue; }
+        properties[n] = JSON.stringify(v);
     }
 
     return {
