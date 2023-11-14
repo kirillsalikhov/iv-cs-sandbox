83037c711494abcefdc6ae4ca77656347a6470d2
kirill <kirill.salikhov@gmail.com>
2023-11-14 19:47:47 +0400

    wip: vite dev
    refs: IV-CS-SUPPORT-T-30

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,6 @@ const bodyParser = require('koa-bodyparser');
 const views = require('koa-views');
 
 const router = require('./routes');
-const viteDevAssets = require('./middlewares/vite-dev-assets');
 
 const port = 3050;
 
