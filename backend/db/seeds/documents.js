03f26a5d35855c4dddbd3d972fcafb057686f20b
kirill <kirill.salikhov@gmail.com>
2023-11-16 14:28:27 +0400

    build: make seeds upload files
    refs: IV-CS-SUPPORT-T-22

--- backend/db/seeds/documents.js
+++ backend/db/seeds/documents.js
@@ -1,16 +1,63 @@
+const path = require('path');
+const {v4: uuid} = require('uuid');
+
+const S3Client = require('../../utils/S3Client');
+
+const seedFile = (name) => path.join(__dirname, '../seed-files', name);
+
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.seed = async function(knex) {
-  // Deletes ALL existing entries
-  await knex('documents').del();
-  // TODO add oss files
-  // TODO updated, created
-  await knex('documents').insert([
-    {id: 1, name: "file-1.ifc", status: "finished"},
-    {id: 2, name: "file-2.ifc", status: "finished"},
-    {id: 3, name: "file-3-failed.ifc", status: "failed"},
-    {id: 4, name: "file-4.ifc", status: "inProgress"},
-  ]);
+exports.seed = async function (knex) {
+    // Deletes ALL existing entries
+    await knex('documents').del();
+
+    const docsData = [
+        {
+            name: "Wellness center Sama.ifc",
+            status: "finished",
+            ifcFile: "Wellness center Sama.ifc",
+            zipFile: "Wellness center Sama.zip"
+        },
+        {
+            name: "failed.ifc",
+            status: "failed",
+            ifcFile: "failed.ifc"
+        },
+        {
+            name: "inProgress.ifc",
+            status: "inProgress",
+            ifcFile: "inProgress.ifc"
+        },
+    ]
+
+    return Promise.all(docsData.map((data) => {
+        const docPromisses = [];
+
+        const doc = {
+            name: data.name,
+            status: data.status,
+            created_at: Date.now(),
+            updated_at: Date.now(),
+        };
+
+        if (data.ifcFile) {
+            doc.source_file = uuid();
+            docPromisses.push(
+                S3Client.uploadFile(
+                    doc.source_file,
+                    seedFile(data.ifcFile)));
+        }
+        if (data.zipFile) {
+            doc.view_file = uuid();
+            docPromisses.push(
+                S3Client.uploadFile(
+                    doc.view_file,
+                    seedFile(data.zipFile)));
+        }
+
+        docPromisses.push(knex('documents').insert(doc));
+        return Promise.all(docPromisses);
+    }));
 };
