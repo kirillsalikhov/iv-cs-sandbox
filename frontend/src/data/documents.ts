c9c68094a0c0d0b6e64de0a162d959e4be4d3528
Max Ustinov <ewclide@yandex.ru>
2023-11-17 15:16:09 +0400

    attach backend api to main page

--- frontend/src/data/documents.ts
+++ frontend/src/data/documents.ts
@@ -1,4 +1,4 @@
-import { backendIsAvailable } from './runtime-environment.ts';
+import axios from 'axios';
 
 export interface DocumentData {
     id: number;
@@ -13,17 +13,57 @@ export const documentsStub: DocumentData[] = [
     {id: 4, name: 'file-4.ifc', status: 'inProgress'},
 ];
 
-export const DOCUMENT_URL = '/api/documents';
+export const DOCUMENTS_URL = '/api/documents';
 
-export async function loadDocuments(): Promise<DocumentData[]> {
-    if (!backendIsAvailable()) {
+export function getInitialDocuments(): DocumentData[] {
+    if (window.forBrowser === undefined) {
         return documentsStub;
     }
 
-    const response = await fetch(DOCUMENT_URL);
-    if (response.ok) {
-        return await response.json() as DocumentData[];;
-    }
+    return window.forBrowser.documents;
+}
 
-    throw new Error(`Error ${response.status}`);
+export async function getDocuments(): Promise<DocumentData[] | null> {
+    try {
+        const response = await axios.get(DOCUMENTS_URL);
+        return response.data as DocumentData[];
+    } catch (err) {
+        alert (err);
+        return null;
+    }
+}
+
+export function download(name: string, ext: string, data: string): void {
+    const a = document.createElement('a');
+    const file = new Blob([data], { type : 'plain/text' });
+    a.href = URL.createObjectURL(file);
+    a.download = `${name}.${ext}`;
+    a.click();
+}
+
+export async function getDocumentSource(id: number): Promise<string | null> {
+    try {
+        const src = await axios.get(`${DOCUMENTS_URL}/${id}/source`);
+        return src.data as string;
+    } catch (err) {
+        alert(err);
+        return null;
+    }
+}
+
+export async function downloadDocument(name: string, id: number) {
+    const src = await getDocumentSource(id);
+    if (src !== null) {
+        download(name, 'ifc', src);
+    }
+}
+
+export async function deleteDocument(id: number): Promise<boolean> {
+    try {
+        await axios.delete(`${DOCUMENTS_URL}/${id}`);
+        return true;
+    } catch (err) {
+        alert(err);
+        return false;
+    }
 }
