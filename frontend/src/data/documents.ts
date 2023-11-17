0412b125dd7cc42dfe25ce43b6ef83f835c0af33
Max Ustinov <ewclide@yandex.ru>
2023-11-17 16:10:42 +0400

    add documents api object

--- frontend/src/data/documents.ts
+++ frontend/src/data/documents.ts
@@ -6,64 +6,111 @@ export interface DocumentData {
     status: 'failed' | 'inProgress' | 'finished';
 }
 
-export const documentsStub: DocumentData[] = [
+const documentsStub: DocumentData[] = [
     {id: 1, name: 'file-1.ifc', status: 'finished'},
     {id: 2, name: 'file-2.ifc', status: 'finished'},
     {id: 3, name: 'file-3-failed.ifc', status: 'failed'},
     {id: 4, name: 'file-4.ifc', status: 'inProgress'},
 ];
 
-export const DOCUMENTS_URL = '/api/documents';
+export class DocumentsAPI {
+    static readonly DOCUMENTS_URL = '/api/documents';
 
-export function getInitialDocuments(): DocumentData[] {
-    if (window.forBrowser === undefined) {
-        return documentsStub;
+    private _documents: Map<number, DocumentData>;
+    private _serverIsAvailable: boolean;
+
+    constructor() {
+        const documents = new Map();
+
+        let serverIsAvailable = false;
+        let docList: DocumentData[] = [];
+        if (window.forBrowser === undefined) {
+            docList = documentsStub;
+            console.warn('Your browser is offline');
+        } else {
+            serverIsAvailable = true;
+            docList = window.forBrowser.documents;
+        }
+
+        for (const doc of docList) {
+            documents.set(doc.id, doc);
+        }
+
+        this._documents = documents;
+        this._serverIsAvailable = serverIsAvailable;
     }
 
-    return window.forBrowser.documents;
-}
-
-export async function getDocuments(): Promise<DocumentData[] | null> {
-    try {
-        const response = await axios.get(DOCUMENTS_URL);
-        return response.data as DocumentData[];
-    } catch (err) {
-        alert (err);
-        return null;
+    get list(): DocumentData[] {
+        return [...this._documents.values()];
     }
-}
 
-export function download(name: string, data: string): void {
-    const a = document.createElement('a');
-    const file = new Blob([data], { type : 'plain/text' });
-    a.href = URL.createObjectURL(file);
-    a.download = name;
-    a.click();
-}
-
-export async function getDocumentSource(id: number): Promise<string | null> {
-    try {
-        const src = await axios.get(`${DOCUMENTS_URL}/${id}/source`);
-        return src.data as string;
-    } catch (err) {
-        alert(err);
-        return null;
+    getLink(id: number): string {
+        const document = this._documents.get(id);
+        if (document !== undefined && document.status === 'finished') {
+            return `${id}/viewer`;
+        }
+        return '#';
     }
-}
 
-export async function downloadDocument(name: string, id: number) {
-    const src = await getDocumentSource(id);
-    if (src !== null) {
-        download(name, 'ifc', src);
+    async delete(id: number): Promise<void> {
+        if (!this._serverIsAvailable) {
+            this._documents.delete(id);
+            return;
+        }
+
+        try {
+            await axios.delete(`${DocumentsAPI.DOCUMENTS_URL}/${id}`);
+            await this._syncDocuments();
+        } catch (err) {
+            alert(err);
+        }
     }
-}
 
-export async function deleteDocument(id: number): Promise<boolean> {
-    try {
-        await axios.delete(`${DOCUMENTS_URL}/${id}`);
-        return true;
-    } catch (err) {
-        alert(err);
-        return false;
+    async download(id: number): Promise<void> {
+        const document = this._documents.get(id);
+        if (document === undefined) {
+            alert(`Can't download non-existent document ${id}`);
+            return;
+        }
+
+        const source = await this._getSource(id);
+        if (source !== null) {
+            this._saveOnDisk(document.name, source);
+        }
+    }
+
+    private _saveOnDisk(name: string, data: string): void {
+        const a = document.createElement('a');
+        const file = new Blob([data], { type : 'plain/text' });
+        a.href = URL.createObjectURL(file);
+        a.download = name;
+        a.click();
+    }
+
+    private async _getSource(id: number): Promise<string | null> {
+        if (!this._serverIsAvailable) {
+            alert(`Can't get document source ${id} because of offline`);
+            return null;
+        }
+
+        try {
+            const source = await axios.get(`${DocumentsAPI.DOCUMENTS_URL}/${id}/source`);
+            return source.data as string;
+        } catch (err) {
+            alert(err);
+            return null;
+        }
+    }
+
+    private async _syncDocuments(): Promise<void> {
+        const response = await axios.get(DocumentsAPI.DOCUMENTS_URL);
+        const documents = response.data as DocumentData[];
+
+        if (documents) {
+            this._documents.clear();
+            for (const doc of documents) {
+                this._documents.set(doc.id, doc);
+            }
+        }
     }
 }
