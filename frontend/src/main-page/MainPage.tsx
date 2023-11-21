94f6e4fa131b95b772aec476acab4f5dfa3ee7e5
Mikhail Turkeev <turkeevm@gmail.com>
2023-11-21 19:16:21 +0400

    extract css in the main page too

--- frontend/src/main-page/MainPage.tsx
+++ frontend/src/main-page/MainPage.tsx
@@ -1,8 +1,9 @@
 import { ConversionType, DocumentData, DocumentsAPI } from '../data/documents.js';
 import { ChangeEvent, useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import './main-page.css';
+import { iconDelete, iconSource } from './icons.tsx';
 
-function Upload({ api }: { api: DocumentsAPI }) {
+function UploadPanel({ api }: { api: DocumentsAPI }) {
     const [conversionType, setConversionType] = useState<ConversionType>(ConversionType.IFC_2_WMD);
     const [isUploading, setUploading] = useState<boolean>(false);
     const [file, setFile] = useState<File | null>(null);
@@ -37,10 +38,10 @@ function Upload({ api }: { api: DocumentsAPI }) {
     }, [file, conversionType, api, isUploading]);
 
     return (
-        <div className="col-span-full h-min m-4 bg-white rounded border border-gray-200 p-4 md:col-span-4 md:order-last md:mr-8">
-            <div className="block mb-6 font-medium text-lg">Add file</div>
+        <>
+            <div className="upload__title">Add file</div>
             <input
-                className="block w-full h-10 mb-4 p-1 items-center text-gray-500 rounded border border-gray-300 cursor-pointer bg-gray-50 hover:bg-gray-100 focus:outline-none"
+                className="upload__input"
                 id="file_input"
                 type="file"
                 onChange={onChangeFile}
@@ -48,7 +49,7 @@ function Upload({ api }: { api: DocumentsAPI }) {
             >
             </input>
             <select
-                className="block w-full h-10 mb-6 py-1.5 px-2 rounded border border-gray-300 cursor-pointer bg-gray-50 hover:bg-gray-100 focus:outline-none "
+                className="upload__select"
                 defaultValue={conversionType}
                 onChange={onChangeSelect}
             >
@@ -57,15 +58,15 @@ function Upload({ api }: { api: DocumentsAPI }) {
             </select>
             <button
                 type="button"
-                className="w-full h-10 rounded bg-blue-700 hover:bg-blue-600 px-2.5 py-1.5 text-white focus-outline-none"
+                className="upload__button"
                 disabled={isUploading}
                 onClick={onClickConversion}
                 style={{ opacity: isUploading ? 0.5 : 1 }}
             >
                 {isUploading ? 'Uploading...' : 'Convert'}
             </button>
-        </div>
-    )
+        </>
+    );
 }
 
 export interface DocumentCardProps {
@@ -79,33 +80,28 @@ function classNames(...classes: (string | null | undefined | false)[]) {
 
 function DocumentCard({ document, onDelete }: DocumentCardProps) {
     return (
-        <div className="flex items-center p-4 gap-4 hover:bg-gray-50 cursor-pointer">
-            <div className="flex w-full h-6 gap-2 ">
-                <div className={classNames(`badge--${document.status}`, 'flex-none sm:order-last')}>{document.status}</div>
+        <div className="document-card">
+            <div className="document-card__name-and-badge">
+                <div className={classNames('document-card__badge', `document-card__badge--${document.status}`)}>{document.status}</div>
                 <a
-                    className={classNames(`docStyle--${document.status}`, 'flex-grow overflow-hidden text-ellipsis ')}
+                    className={classNames('document-card__name', `document-card__name--${document.status}`)}
                     href={document.viewerLink}
                 >{document.name}</a>
             </div>
             <a
                 title="Download source file"
-                className="flex flex-none items-center rounded bg-white hover:bg-gray-100 text-gray-400 text-sm hover:text-gray-800 leading-5 p-2.5 gap-1"
-                href={document.sourceLink}
-            >
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
-                </svg>
-                <div className="hidden sm:block">Source</div>
+                className="document-card__source-button"
+                href={document.sourceLink}>
+                {iconSource}
+                <div className="document-card__source-button-text">Source</div>
             </a>
             <button
                 type="button"
                 title="Delete file"
-                className="flex-none items-center rounded bg-white hover:bg-gray-100 text-gray-400 hover:text-gray-800 leading-5 p-2.5"
+                className="document-card__delete-button"
                 onClick={() => onDelete(document.id)}
             >
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                </svg>
+                {iconDelete}
             </button>
         </div>
     )
@@ -125,22 +121,13 @@ export function MainPage() {
     const onDelete = useCallback((id: number) => documentsAPI.delete(id), [documentsAPI]);
 
     return (
-        <div className="bg-white h-screen">
-            <div className="container m-auto max-w-screen-2xl grid grid-cols-12">
-
-
-                {/* Heading */}
-                <div className="col-span-full m-4 md:m-8">
-                    <h1>Root page (delete this heading)</h1>
+        <div className="main-page">
+            <div className="main-page__container">
+                <div className="main-page__upload">
+                    <UploadPanel api={documentsAPI} />
                 </div>
 
-
-                {/* Upload panel */}
-                <Upload api={documentsAPI} />
-
-
-                {/* Documents */}
-                <div className="col-span-full min-w-fit m-4 bg-white rounded border border-gray-200 md:col-span-8 md:ml-8 ">
+                <div className="main-page__documents">
                     <div className="divide-y divide-gray-200 md:p-4">
                         {documents.map((document) => (
                             <DocumentCard key={document.id} document={document} onDelete={onDelete} />
--- frontend/src/main-page/index.tsx
+++ frontend/src/main-page/index.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
-import { MainPage } from './MainPage.tsx';
 import '../index.css';
+import { MainPage } from './MainPage.tsx';
 
 const reactRootElement = document.getElementById('react-root') as HTMLDivElement;
 const reactRoot = createRoot(reactRootElement);
