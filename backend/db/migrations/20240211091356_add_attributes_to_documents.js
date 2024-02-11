f92b1ce95e47eff32e7e79f16c01187664958212
kirill <kirill.salikhov@gmail.com>
2024-02-11 13:24:23 +0400

    wip(attributes_file): add migration refs: IV-CS-SUPPORT-T-94

---
+++ backend/db/migrations/20240211091356_add_attributes_to_documents.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema
+        .table('documents', (table) => {
+            table.uuid('attributes_file');
+        })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema
+        .table('documents', (table) => {
+            table.dropColumn('attributes_file');
+        })
+};
